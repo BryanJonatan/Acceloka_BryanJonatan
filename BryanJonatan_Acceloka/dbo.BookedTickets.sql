3cd6127c4aab46480de4640345edec5c8cb5c72c
BRYAN JONATAN <bryan.jonatan@binus.ac.id>
2025-02-14 10:53:48 +0700

    Initial commit

---
+++ BryanJonatan_Acceloka/dbo.BookedTickets.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BookedTickets] (
+    [BookingId]  VARCHAR (255) NOT NULL,
+    [TicketCode] VARCHAR (255) NOT NULL,
+    [Quantity]   INT           DEFAULT ((1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([BookingId] ASC),
+    FOREIGN KEY ([TicketCode]) REFERENCES [dbo].[Tickets] ([TicketCode])
+);
+
---
+++ BryanJonatan_Acceloka/dbo.Ticket.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Ticket] (
+    [TicketCode]   VARCHAR (255) NOT NULL,
+    [TicketName]   VARCHAR (255) NOT NULL,
+    [CategoryName] VARCHAR (255) NOT NULL,
+    [Quota]        INT           NOT NULL,
+    [EventDate]    DATETIME      NOT NULL,
+    [Price]        INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([ticketCode] ASC)
+);
+
